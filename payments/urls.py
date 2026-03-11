6d1e71690157912c220f588450f0dfe2742fe359
Gideon <ekibade.gideon@yahoo.comm>
2026-03-11 06:17:06 +0100

    updating some files

--- payments/urls.py
+++ payments/urls.py
@@ -1,26 +1,3 @@
-
-# from django.urls import path
-# from .views import PaystackInitializeAPIView, PaystackVerifyAPIView
-
-# # urlpatterns = [
-# #     path("paystack/init/", PaystackInitializeAPIView.as_view(), name="paystack-init"),
-
-
-# #     path("paystack/verify/<str:reference>/", PaystackVerifyAPIView.as_view(), name="paystack-verify"),
-
-# #    path("webhook/", PaystackVerifyAPIView.as_view(), name="paystack-webhook"),
-
-# # ]
-
-
-# urlpatterns = [
-#     path("paystack/init/", PaystackInitializeAPIView.as_view(), name="paystack-init"),
-#     path("paystack/verify/<str:reference>/", PaystackVerifyAPIView.as_view(), name="paystack-verify"),
-#     path("webhook/", PaystackWebhookAPIView.as_view(), name="paystack-webhook"),
-# ]
-
-
-
 from django.urls import path
 from .views import PaystackInitializeAPIView, PaystackVerifyAPIView, PaystackWebhookAPIView
 
--- realestate/urls.py
+++ realestate/urls.py
@@ -35,10 +35,3 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
     )
-
-
-
-    # urlpatterns += static(
-    # settings.MEDIA_URL,
-    # document_root=settings.MEDIA_ROOT
-    # )
