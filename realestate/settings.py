9c27f8b4c1f390130ad49f351a437cc095e2a31c
Gideon <ekibade.gideon@yahoo.comm>
2026-05-26 23:09:02 +0100

    Correcting my  email host password

--- realestate/settings.py
+++ realestate/settings.py
@@ -185,6 +185,9 @@ EMAIL_HOST_USER = 'gekibade@gmail.com'
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "gekibade@gmail.com")
+FRONTEND_URL = os.getenv("FRONTEND_URL", "https://estatehhub.netlify.app")
+
 
 # Default Primary Key
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
