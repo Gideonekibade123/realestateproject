b70df5d1e22dcb165cb6a060f8d4470264eb2d73
Gideon <ekibade.gideon@yahoo.comm>
2026-05-28 23:16:08 +0100

    Adding the custom profile

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Profile
 
-# Register your models here.
 
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ['username', 'email', 'is_active', 'is_verified', 'is_staff']
+    list_filter = ['is_active', 'is_verified', 'is_staff']
+    search_fields = ['username', 'email']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Verification', {'fields': ('is_verified',)}),
+    )
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'phone_number', 'payment_completed']
+    search_fields = ['user__username', 'user__email']
