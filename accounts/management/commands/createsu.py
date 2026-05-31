98fca66200a876b39b3e59439d912a77689485df
Gideon <ekibade.gideon@yahoo.comm>
2026-05-31 21:56:06 +0100

    fix: force reset superuser password on deploy

--- accounts/management/commands/createsu.py
+++ accounts/management/commands/createsu.py
@@ -1,19 +1,27 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+import os
 
 User = get_user_model()
 
 
 class Command(BaseCommand):
-    help = 'Create superuser automatically'
+    help = 'Create or update superuser automatically'
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username='Gideon').exists():
-            User.objects.create_superuser(
-                username='Gideon',
-                email='gekibade@gmail.com',
-                password='Gideon@1234'
-            )
+        username = os.getenv("DJANGO_SUPERUSER_USERNAME", "Gideon")
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL", "gekibade@gmail.com")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "Gideon@1234")
+
+        user, created = User.objects.get_or_create(username=username)
+        user.email = email
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_active = True
+        user.set_password(password)  # ✅ always resets password on deploy
+        user.save()
+
+        if created:
             self.stdout.write('Superuser created!')
         else:
-            self.stdout.write('Superuser already exists!')
+            self.stdout.write('Superuser updated!')
